c42f0f5431446e41669d34b5266517b48191c477
CHRIS BUSHONG <chris.bushong@gmail.com>
2019-02-20 18:18:33 -0800

    add episode 1

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,16 @@ const Header = () => (
   <header className="header">
     <div className="header__left">
       <Link href="/">
-        <a aria-label="Syntax.FM">
+        <a aria-label="TaxDiner.com">
           <img className="header__logo" src="/static/tax-diner.svg" alt="Tax Diner" />
         </a>
       </Link>
     </div>
     <div className="header__right">
     
+      <div className="title" />
       <div className="title">
-        <h2 className="tagline">Where Tax Professionals Chew the Fat</h2>
+        <h2 className="tagline">The Podcast Where Tax Professionals Chew the Fat</h2>
       </div>
       
     </div>
---
+++ shows/001 - Michael Diyanni of La Jolla Tax.md
@@ -0,0 +1,10 @@
+---
+number: 0
+title: Episode 1 - Michael Diyanni of La Jolla Tax
+date: 1550671200938
+url: https://s3.us-west-1.wasabisys.com/tax-diner/001 Mike Diyanni.mp3
+---
+
+## Show Notes
+
+Here's some test stuff
